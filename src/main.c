c279b6abf72ff5e3a5a660fdb6137505dd6e9124
Christopher Bertels <bakkdoor@flasht.de>
2010-01-04 02:36:30 +0100

    added stdlib directory containing standard library functions written in generica itself

--- src/main.c
+++ src/main.c
@@ -6,18 +6,39 @@ int yyparse();
 
 scope *global_scope;
 
+#define STDLIB_FILES {"stdlib/lists.gna"}
+#define N_STDLIB_FILES 1
+
+void parse_file(char *filename)
+{
+  if(freopen(filename, "r", stdin) == NULL)
+  {
+    fprintf(stderr, "File %s cannot be opened.\n", filename);
+    exit(1);
+  }
+  
+  yyparse();
+}
+
 int main(int argc, char **argv)
 {
+  int i;
+  char *files[] = STDLIB_FILES;
+
+  global_scope = new_scope(NULL);
+  init_global_objects();
+  init_global_scope();
+
+  for(i = 0; i < N_STDLIB_FILES; i++) {
+    parse_file(files[i]);
+  }
+  
   if ((argc > 1) && (freopen(argv[1], "r", stdin) == NULL))
   {
     fprintf(stderr, "%s: File %s cannot be opened.\n", argv[0], argv[1]);
     exit(1);
   }
-  
-  global_scope = new_scope(NULL);
-  init_global_objects();
-  init_global_scope();
-  
+    
   yyparse();
 
   return 0;
